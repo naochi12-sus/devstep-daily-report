20df733c0e11ac56557ac63dd382fc04c64dfe91
naochi12-sus <nao.on.22.my@gmail.com>
2026-04-23 02:03:22 +0900

    fix:TypeScriptのためのコード訂正

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,15 @@ import path from "path";
 export default defineConfig({
     plugins: [react()],
     test: {
+        exclude: [
+            "**/node_modules/**",
+            "**/dist/**",
+            "my-e2e-test/**", // ← ここにPlaywrightのフォルダ名を追加！
+        ],
+        coverage: {
+            provider: "v8", // または 'istanbul'
+            reporter: ["text", "json", "html"],
+        },
         environment: "jsdom", // ブラウザ環境をシミュレート
         globals: true, // expectなどをグローバルに使えるようにする
         setupFiles: "./vitest.setup.ts",
